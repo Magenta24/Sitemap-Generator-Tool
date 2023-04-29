8c96ea696c13c0bc7acbfde18bcfeec807c8e4e5
Jagoda Tata <sc20jmt@leeds.ac.uk>
2023-04-29 21:03:54 +0100

    adding tests

--- sitemap_generator/sitemap_generator/tests.py
+++ sitemap_generator/sitemap_generator/tests.py
@@ -1,13 +1,13 @@
 # from django.test import TestCase
 import unittest
 from URLSanitizer import URLSanitizer
+from RobotsHandler import RobotsHandler
 
 
 class TestURLSanitizer(unittest.TestCase):
 
     def test_sanitize_url(self):
         self.assertEqual(URLSanitizer.sanitize_url("/xd", "https://wp.pl"), "https://wp.pl/xd")
-        # self.assertEqual(URLSanitizer.sanitize_url("https://crawler-test.com//urls/double_slash/disallowed_start", "https://crawler-test.com"), None)
         self.assertEqual(URLSanitizer.sanitize_url("https://crawler-test.com/?parameter-on-hostname-root=parameter-value", "https://crawler-test.com"), "https://crawler-test.com/?parameter-on-hostname-root=parameter-value")
         self.assertEqual(URLSanitizer.sanitize_url("/", "https://www.test.com"), "https://www.test.com")
 
@@ -24,15 +24,19 @@ class TestURLSanitizer(unittest.TestCase):
     # def test_is_image_by_extension(self):
     #     pass
     #
-    # def test_is_image_by_header(self):
-    #     pass
+    def test_is_image_by_header(self):
+        pass
     #
     # def test_is_doc_by_extension(self):
     #     pass
     #
-    # def test_is_doc_by_header(self):
-    #     pass
+    def test_is_doc_by_header(self):
+        pass
 
+class TestRobotsHandler(unittest.TestCase):
+    def test_join_robots_txt(self):
+        self.assertEqual(RobotsHandler.join_robots_txt("https://en.wikipedia.org"), "https://en.wikipedia.org/robots.txt")
+        self.assertEqual(RobotsHandler.join_robots_txt("https://en.wikipedia.org/something/"), "https://en.wikipedia.org/robots.txt")
 
 if __name__ == '__main__':
     unittest.main()
